76b7636e3cfc2295b253d6af06a834ef5b5f3051
Samuel Simplicio <samuelsimplicio5@gmail.com>
2022-07-05 23:36:01 -0300

    Added data offset explanation

--- go_to_pos.sh
+++ go_to_pos.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # goes to the oil position
 
+# when published the drone position is:
+# x: -9.098373 y: 20.425953 z: 15.298266
+# so we have an offset to account for
 rostopic pub -1 /go_to_pos geometry_msgs/Pose "position:
   x: -10.0
   y: 20.0
